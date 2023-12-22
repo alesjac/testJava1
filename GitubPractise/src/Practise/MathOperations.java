2942f453bde571f23166c314b7d0b8cb72767514
alesj <alesj@10.0.0.137>
2023-12-22 12:34:16 -0500

    Division of numbers.

--- GitubPractise/src/Practise/MathOperations.java
+++ GitubPractise/src/Practise/MathOperations.java
@@ -7,6 +7,9 @@ public class MathOperations {
 		int number2 = 10;
 		int sum = number1 +number2;
 		System.out.println("Sum of numbers is "+ sum);
+		
+		int division = number2 /number1;
+		System.out.println("Division of numbers is "+ division);
 
 	}
 
