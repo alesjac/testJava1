906c69e2d4900ce7213c8cb4166af712a3272544
alesj <alesj@10.0.0.137>
2023-12-22 12:07:41 -0500

    Sum of numbers

---
+++ GitubPractise/src/Practise/MathOperations.java
@@ -0,0 +1,13 @@
+package Practise;
+
+public class MathOperations {
+
+	public static void main(String[] args) {
+		int number1 = 5;
+		int number2 = 10;
+		int sum = number1 +number2;
+		System.out.println("Sum of numbers is "+ sum);
+
+	}
+
+}
